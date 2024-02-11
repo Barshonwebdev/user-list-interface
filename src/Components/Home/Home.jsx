e643128699cdffef445f107880294b6510cf2a94
Barshon <barshonweb@gmail.com>
2024-02-11 18:04:03 +0600

    card shown responsive (mobile + tablet)

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import UserCard from "../UserCard/UserCard";
+import { key } from "localforage";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
@@ -10,7 +12,19 @@ const Home = () => {
       });
   }, []);
   console.log(users);
-  return <div>ghjk</div>;
+
+  return (
+    <div>
+      <p className="text-center my-8 text-3xl">User List Interface</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-5 my-6">
+        {
+          users.map(user =>
+            <UserCard key={user.id}></UserCard>
+          )
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Home;
