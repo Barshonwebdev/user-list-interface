28e765f47908293ee9356147e985e8c1ece49e94
Barshon <barshonweb@gmail.com>
2024-02-12 11:22:26 +0600

    searchbar design

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import UserCard from "../UserCard/UserCard";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
@@ -15,6 +16,7 @@ const Home = () => {
   return (
     <div className="">
       <p className="text-center my-9  text-3xl">User List Interface</p>
+      <SearchBar></SearchBar>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-5 mb-6">
         {users.map((user) => (
           <UserCard user={user} key={user.id}></UserCard>
---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="mx-7 my-5">
+      <div className="form-control">
+        <input
+          type="text"
+          placeholder="Search User"
+          className="input input-bordered md:w-1/4 lg:w-1/5 w-1/2"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
